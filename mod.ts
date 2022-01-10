26d303578c73205a8708ca23f1d71c690c8a1077
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2022-01-10 10:44:41 +0630

    feat: add entry komando function

--- mod.ts
+++ mod.ts
@@ -1,5 +1,35 @@
+import { Args, parse } from 'https://deno.land/std@0.120.0/flags/mod.ts';
+
+export function komando(opt: KomandoOptions, argv: string[] = Deno.args) {
+  const resolved: ResolvedKomandoOptions = {
+    commands: [],
+    flags: {
+      help: {
+        help: 'Show this message',
+        alias: 'h',
+        preserve: true,
+      },
+    },
+    args: {},
+    ...opt,
+  };
+
+  if (opt.version) {
+    resolved.flags.version = {
+      help: 'Show version info',
+      alias: 'V',
+      preserve: true,
+    };
+  }
+
+  run(resolved, parse(argv));
+}
+
 export function defineCommand(command: Command): Command {
   return command;
+}
+
+function run(resolved: ResolvedKomandoOptions, argv: Args) {
 }
 
 type Command = {
@@ -17,6 +47,17 @@ type Command = {
 type KomandoOptions = Omit<Command, 'alias'> & {
   version: string;
 };
+
+type RequireOnly<T, Keys extends keyof T> =
+  & Pick<T, Exclude<keyof T, Keys>>
+  & {
+    [K in Keys]-?: T[K];
+  };
+
+type ResolvedKomandoOptions = RequireOnly<
+  KomandoOptions,
+  'commands' | 'flags' | 'args'
+>;
 
 type Flag = {
   [field: string]: {
